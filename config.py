df238056d30afa50f557bee4eaa0e78a3e2a4835
snydecor000 <cory.a.snyder@gmail.com>
2022-01-31 00:49:22 +0000

    Added config file. Future changes will not be tracked

---
+++ config.py
@@ -0,0 +1,13 @@
+#config.py
+
+# BeagleBone Black LCD pin configuration:
+lcd_rs        = 'P8_8'
+lcd_en        = 'P8_10'
+lcd_d4        = 'P8_12'
+lcd_d5        = 'P8_14'
+lcd_d6        = 'P8_16'
+lcd_d7        = 'P8_18'
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows    = 2
